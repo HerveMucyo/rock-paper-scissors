9aba756f395ddfe7146fde6fdad1e96d3c744ad2
Herve Mucyo <mucyoh32@gmail.com>
2023-07-08 12:29:29 +0200

    Edited README.md, index.html and index.js

--- index.js
+++ index.js
@@ -1,25 +1,61 @@
-function startGame() {
-    cm = Math.floor(Math.random() * 3);
+// Function to get the computer's choice
+function getComputerChoice() {
+    return Math.floor(Math.random() * 3);
+}
 
-    input = prompt("Enter your Choice!");
+// Function to play a single round of Rock Paper Scissors
+function playRound(playerSelection, computerSelection) {
+    // Convert playerSelection to an integer
+    playerSelection = parseInt(playerSelection);
 
-    pm = parseInt(input);
-
-    if (cm == 0 && pm == 1) {
-        alert("You beat Computer with Paper against Rock");
-    } else if (cm == 0 && pm == 2) {
-        alert("Computer Beat you with Rock");
-    } else if (cm == 1 && pm == 0) {
-        alert("Computer Beat you With Paper");
-    } else if (cm == 1 && pm == 2) {
-        alert("You beat computer with Scissor against Paper");
-    } else if (cm == 2 && pm == 0) {
-        alert("You beat computer with Rock against Scissor");
-    } else if (cm == 2 && pm == 1) {
-        alert("Computer Beat you With Scissor");
+    // Determine the winner
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (
+        (playerSelection === 0 && computerSelection === 2) ||
+        (playerSelection === 1 && computerSelection === 0) ||
+        (playerSelection === 2 && computerSelection === 1)
+    ) {
+        return "You win!";
     } else {
-        alert("It's a tie");
+        return "You lose!";
     }
 }
 
+// Function to play a 5 round game
+function startGame() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let round = 1; round <= 5; round++) {
+        const playerSelection = prompt("Enter your choice: 0 for Rock, 1 for Paper, 2 for Scissors");
+        const computerSelection = getComputerChoice();
+        const result = playRound(playerSelection, computerSelection);
+
+        console.log(`Round ${round}: ${result}`);
+        alert(`Round ${round}: ${result}`);
+
+        if (result === "You win!") {
+            playerScore++;
+        } else if (result === "You lose!") {
+            computerScore++;
+        }
+    }
+
+    console.log(`Final Score - You: ${playerScore}, Computer: ${computerScore}`);
+    alert(`Final Score - You: ${playerScore}, Computer: ${computerScore}`);
+
+    if (playerScore > computerScore) {
+        console.log("Congratulations! You win the game!");
+        alert("Congratulations! You win the game!");
+    } else if (playerScore < computerScore) {
+        console.log("You lose the game! Better luck next time.");
+        alert("You lose the game! Better luck next time.");
+    } else {
+        console.log("It's a tie! The game ends in a draw.");
+        alert("It's a tie! The game ends in a draw.");
+    }
+}
+
+// Start the game
 startGame();
